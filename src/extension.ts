0138d0f4d01e92d0fe4f4e54056efde749428ff0
wumo <760478279@qq.com>
2025-08-21 16:23:06 +0800

    feat: 支持@Options定义的组件的索引

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from 'vscode'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import { parseVueClassComponents } from './parser'
+
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vue-decorator-extension" is now active!');
+  const provider = vscode.languages.registerDefinitionProvider(
+    { language: 'vue' },
+    {
+      provideDefinition(document, position) {
+        const wordRange = document.getWordRangeAtPosition(
+          position,
+          /[A-Za-z0-9_]+/
+        )
+        if (!wordRange) return null
+        const word = document.getText(wordRange)
+        const filePath = document.uri.fsPath
+        const map = parseVueClassComponents(filePath)
+        if (map[word]) {
+          return new vscode.Location(
+            vscode.Uri.file(map[word]),
+            new vscode.Position(0, 0)
+          )
+        }
+        return null
+      }
+    }
+  )
 
-	// The command has been defined in the package.json
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vue-decorator-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vue-decorator-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(provider)
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
